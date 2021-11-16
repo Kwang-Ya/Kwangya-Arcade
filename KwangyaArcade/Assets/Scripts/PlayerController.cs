ab039ce03058d89f3d74ac85488c904890e8e3b6
Jy-do <82003899+Jy-do@users.noreply.github.com>
2021-11-16 17:28:24 +0900

    spike 처리
    spike인거 인지

---
+++ KwangyaArcade/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+
+    {
+        int heart = 3;
+
+        if (collision.gameObject.tag == "Spike")
+
+        {
+            heart-=1;
+            print(heart);
+        }
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
