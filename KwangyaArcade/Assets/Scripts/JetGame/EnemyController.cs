9a465434ab1be6dcbaf9433566c33afbedd9c0e4
miiinnn23 <miiinnn23@users.noreply.github.com>
2021-11-21 16:56:00 +0900

    Add Jet game Packages

---
+++ KwangyaArcade/Assets/Scripts/JetGame/EnemyController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    GameObject scoreUIText;
+
+    GameObject player;
+
+    float speed;
+    public int health;
+    public GameObject Explosion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 2f;
+
+        player = GameObject.Find("Player");
+        scoreUIText = GameObject.FindGameObjectWithTag("ScoreTextTag");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 position = transform.position;
+
+        position = new Vector2(position.x, position.y - speed * Time.deltaTime);
+        transform.position = position;
+
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+
+        if(transform.position.y < min.y)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "PlayerBulletTag")
+        {
+            PlayExplosion();
+            health -= player.GetComponent<PlayerControl>().Attack;
+            scoreUIText.GetComponent<GameScore>().Score += 100;
+
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    void PlayExplosion()
+    {
+        GameObject explosion = (GameObject)Instantiate(Explosion);
+        explosion.transform.position = transform.position;
+    }
+}
---
+++ KwangyaArcade/Assets/Scripts/JetGame/GameManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject playerShip;
+    public GameObject buttonPlay;
+
+    public GameObject enemySpawner;
+    public GameObject enemySpawner2;
+    public GameObject enemySpawner3;
+
+    public GameObject scoreUIText;
+    public GameObject gameClearText;
+
+    public enum GameManagerState
+    {
+        Opening,
+        Gameplay,
+        Gameover,
+        Gameclear,
+    }
+
+    GameManagerState GMState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GMState = GameManagerState.Opening;
+    }
+
+    void UpdateGameManagerState()
+    {
+        switch(GMState)
+        {
+            case GameManagerState.Opening:
+                buttonPlay.SetActive(true);
+                gameClearText.SetActive(false);
+                break;
+
+            case GameManagerState.Gameplay:
+                scoreUIText.GetComponent<GameScore>().Score = 0;
+
+                buttonPlay.SetActive(false);
+                playerShip.GetComponent<PlayerControl>().Init();
+
+                enemySpawner.GetComponent<EnemySpawner>().ScheduleEnemySpawner();
+                enemySpawner2.GetComponent<EnemySpawner2>().ScheduleEnemySpawner();
+                enemySpawner3.GetComponent<EnemySpawner3>().ScheduleEnemySpawner();
+                break;
+
+            case GameManagerState.Gameover:
+                enemySpawner.GetComponent<EnemySpawner>().UnscheduleEnemySpawner();
+                enemySpawner2.GetComponent<EnemySpawner2>().UnscheduleEnemySpawner();
+                enemySpawner3.GetComponent<EnemySpawner3>().UnscheduleEnemySpawner();
+
+                Invoke("ChangeToOpeningState", 8f);
+                break;
+
+            case GameManagerState.Gameclear:
+                gameClearText.SetActive(true);
+
+                Invoke("ChangeToOpeningState", 8f);
+                break;
+        }
+    }
+
+    public void SetGameManagerState(GameManagerState state)
+    {
+        GMState = state;
+        UpdateGameManagerState();
+    }
+
+    public void StartGamePlay()
+    {
+        GMState = GameManagerState.Gameplay;
+        UpdateGameManagerState();
+    }
+
+    public void ChangeToOpeningState()
+    {
+        SetGameManagerState(GameManagerState.Opening);
+    }
+}
