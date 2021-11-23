553347e1132d61eaed7ad50522ef6505b0527704
MinHeeCha <69028785+MinHeeCha@users.noreply.github.com>
2021-11-23 20:13:21 +0900

    이름은 다시 기본틀

--- KwangyaArcade/Assets/Scripts/NameIs/Alphabet.cs
+++ KwangyaArcade/Assets/Scripts/NameIs/Alphabet.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Alphabet : MonoBehaviour
 {
     public char[] alphabetArr;
     public Text alphabetText;
@@ -15,7 +15,18 @@ public class NewBehaviourScript : MonoBehaviour
 
     public void InitializeArr()
     {
+        alphabetArr = new char[27];
+        alphabetText.text = "";
 
+        for (int i = 0; i < 27; i++)
+        {
+            if (i == 26)
+                alphabetArr[i] = '>';
+            else
+                alphabetArr[i] = (char)(65 + i);
+
+            alphabetText.text += alphabetArr[i];
+        }
     }
 
     // Update is called once per frame
