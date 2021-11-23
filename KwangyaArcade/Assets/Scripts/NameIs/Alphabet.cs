bece4b132a34df26dc00b0f6bf4855ba1a0cddf5
MinHeeCha <69028785+MinHeeCha@users.noreply.github.com>
2021-11-23 19:51:49 +0900

    다시 이름은 커밋1

---
+++ KwangyaArcade/Assets/Scripts/NameIs/Alphabet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public char[] alphabetArr;
+    public Text alphabetText;
+
+    void Start()
+    {
+        InitializeArr();
+    }
+
+    public void InitializeArr()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
