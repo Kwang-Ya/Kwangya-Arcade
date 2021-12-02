d3837ee9623f6d24d651aca4d2350e66a08a4788
miiinnn23 <miiinnn23@users.noreply.github.com>
2021-12-02 14:48:13 +0900

    계산기 기초 작동
    완료

--- KwangyaArcade/Assets/Scripts/Bishibashi/Calculator/CalculatorController.cs
+++ KwangyaArcade/Assets/Scripts/Bishibashi/Calculator/CalculatorController.cs
@@ -7,6 +7,12 @@ public class CalculatorController : MonoBehaviour
 {
     public ArrowMoving arrow;
     public Text numberText;
+    public Text noticeText;
+    public Text questionText;
+    public Text timerText;
+
+    public GameObject TimerText;
+    private float time = 60f;
 
     string answer = "";
     string[] calculator =
@@ -17,27 +23,97 @@ public class CalculatorController : MonoBehaviour
         "equal", ".", "0"
     };
 
+    string[] question1 =
+    {
+        "3x2=?", "7x4=?", "15+7=?", "50÷4=?", "36-17=?", "2+3=?", "9x3=?", "4+9=?", "17÷2=?", "4x3=?",
+         "4+29=?", "17÷2=?", "9-6=?", "12x11=?", "17÷2=?", "20÷5=?", "6÷3", "5x15=?", "1+1=?", "10+24=?"
+    };
+    string[] answer1 =
+    {
+        "6", "28", "22", "12.5", "19", "5", "27", "13", "8.5", "12",
+        "33", "8.5", "3", "132", "8.5", "4", "2", "75", "2", "34"
+    };
+
+    string[] question2 =
+    {
+        "429", "609", "210", "403", "323", "1211", "1024", "007", "123", "119",
+        "369", "000", "111", "777", "4.5", "117", "1126", "615", "516", "1214"
+    };
+
+    int questionIndex;
+    int answerCount = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        CreateQuestion();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("down"))
+        if(time > 0)
         {
-            if(arrow.moveCount == 9)
-            {
-                answer = "";
-            }
-            else
-            {
-                answer += calculator[arrow.moveCount];
-            }
+            time -= Time.deltaTime;
 
-            numberText.text = answer;
+            if (Input.GetKeyDown("down"))
+            {
+                if(arrow.moveCount == 9)
+                {
+                    JudgeAnswer();
+                    answer = "";
+                }
+                else
+                {
+                    answer += calculator[arrow.moveCount];
+                }
+
+                numberText.text = answer;
+            }
+        }
+        else
+        {
+            time = 0f;
+            //gameOverText.SetActive(true);
+        }
+
+        timerText.text = string.Format("{0:N2}", time);
+    }
+
+    void CreateQuestion()
+    {
+        questionIndex = Random.Range(0, 20);
+        Debug.Log(questionIndex);
+
+        if(answerCount % 2 == 0)
+        {
+            noticeText.text = "연산하라!";
+            questionText.text = question1[questionIndex];
+        }
+        else
+        {
+            noticeText.text = "빨리 입력하라!";
+            questionText.text = question2[questionIndex];
+        }
+    }
+
+    void JudgeAnswer()
+    {
+        if(answerCount % 2 == 0)
+        {
+            if(answer == answer1[questionIndex])
+            {
+                answerCount++;
+                CreateQuestion();
+            }
+        }
+        else
+        {
+            if (answer == question2[questionIndex])
+            {
+                answerCount++;
+                CreateQuestion();
+            }
         }
     }
 }
